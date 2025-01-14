dd5ea3783e02a6b92085f363ffbecec341d342e0
HannesT117 <12032626+HannesT117@users.noreply.github.com>
2025-01-14 17:56:41 +0100

    fix: adjust mobile view of konzerte
    - remove shadow on mobile
    - remove unnecessary padding

--- src/routes/konzerte/+page.svelte
+++ src/routes/konzerte/+page.svelte
@@ -13,7 +13,7 @@ import ArrowRight from '$lib/components/icons/ArrowRight.svelte';
 <CpcH2 tag="h1">Konzerte</CpcH2>
 <div class="flex flex-col w-full md:w-2/3 lg:w-1/2 justify-center gap-8">
 	{#if data.futureKonzerte.length}
-	<div class="w-full gap-6 rounded-lg bg-white p-6 shadow-md">
+	<div class="w-full gap-6 rounded-lg bg-white p-6 md:shadow-md">
 		<h2 class="mb-4 text-lg font-semibold text-gray-900">Bevorstehende Konzerte</h2>
 		<ul>
 			{#each data.futureKonzerte as konzert}
@@ -31,12 +31,12 @@ import ArrowRight from '$lib/components/icons/ArrowRight.svelte';
 	</div>
 	{/if}
 	{#if data.pastKonzerte.length}
-	<div class="w-full gap-6 rounded-lg bg-white p-6 shadow-md">
+	<div class="w-full gap-6 rounded-lg bg-white p-6 md:shadow-md">
 		<h2 class="mb-4 text-lg font-semibold text-gray-900">Vergangene Konzerte</h2>
 		<ul>
 			{#each data.pastKonzerte as konzert}
 				<a href="konzerte/{konzert.slug}" class="[&:not(:last-child)>*]:border-b-2 ">
-					<li class="border-gray-200 grid grid-cols-konzerte items-center gap-8 p-4">
+					<li class="border-gray-200 grid grid-cols-konzerte items-center gap-8">
 						<div>
 							<div class="text-sm text-gray-500">{konzert.time}</div>
 							<div>{konzert.place}</div>
