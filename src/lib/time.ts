946831bc54f2405bbb4f80ca795970bb85d22715
HannesT117 <12032626+HannesT117@users.noreply.github.com>
2025-02-02 12:22:51 +0100

    fix: correctly handle dates

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -1,7 +1,14 @@
 const formatter = new Intl.DateTimeFormat('de-DE', {
 	timeStyle: 'short',
-	dateStyle: 'long'
+	dateStyle: 'long',
 });
 
-export const createTimeString = (start: string, end: string) =>
-	formatter.formatRange(new Date(start), new Date(end));
+const withoutTimezone = (date: string) => new Date(new Date(date).getTime() - new Date().getTimezoneOffset())
+
+/**
+ * 
+ * @param start The Start Date in the format YYYY-MM-DDTHH:MM
+ * @param end The End Date in the format YYYY-MM-DDTHH:MM
+ * @returns a German string representation of the given time range
+ */
+export const createTimeString = (start: string, end: string) => formatter.formatRange(withoutTimezone(start), withoutTimezone(end));
