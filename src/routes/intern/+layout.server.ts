912854c9fac27f0dc34cf60921b007ffbaf20e72
HannesT117 <12032626+HannesT117@users.noreply.github.com>
2025-03-06 20:03:26 +0100

    fix: use csr for all internal pages

--- src/routes/intern/+layout.server.ts
+++ src/routes/intern/+layout.server.ts
@@ -1,6 +1,8 @@
 import { INTERNAL_TOKEN } from '$env/static/private';
 import { redirect, type ServerLoad } from '@sveltejs/kit';
 
+export const prerender = false;
+
 export const load: ServerLoad = async ({ cookies, url }) => {
 	if (url.searchParams.get('token') !== INTERNAL_TOKEN && cookies.get('token') !== INTERNAL_TOKEN) {
 		redirect(307, '/');
